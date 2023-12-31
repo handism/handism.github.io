ef1a3871bc56ed50b80fcf4ff73e15d45122bd8b
handism <andy2roidy@gmail.com>
2024-01-01 02:08:29 +0900

    タグ要素内で改行されるのを抑止

--- .vitepress/theme/components/ArticleHeader.vue
+++ .vitepress/theme/components/ArticleHeader.vue
@@ -6,8 +6,8 @@ const { frontmatter, page} = useData()
 
 <template>
   <div class="entry-meta">
-    <h1 v-if="frontmatter.title">{{ frontmatter.title }}</h1>
     <time v-if="frontmatter.title">{{ "⌚" + new Date(page.lastUpdated).toLocaleDateString({timeZone: 'Asia/Tokyo'}) }}</time>
+    <h1 v-if="frontmatter.title" class="entry-title">{{ frontmatter.title }}</h1>   
     <span v-for="tag in frontmatter.tags" class="tags">
       <a :href="`/tag/${tag}`" class="tag">{{ "# " + tag }}</a>
     </span>

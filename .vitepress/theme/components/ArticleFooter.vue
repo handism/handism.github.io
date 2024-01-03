e39968497082e496cf0e1242b1fb0785ace71804
handism <andy2roidy@gmail.com>
2024-01-04 03:10:52 +0900

    前後の記事に移動するボタンを追加

--- .vitepress/theme/components/ArticleFooter.vue
+++ .vitepress/theme/components/ArticleFooter.vue
@@ -6,14 +6,10 @@ const { page } = useData()
 
 <template>
   <div class="entry-footer">
-    <span class="blog-navigation" v-for="(post, i) in posts">
-      <div class="navigation-button previous" v-if="i - 1 in posts && posts[i - 1].frontmatter.title && post.url.replace('/', '') == page.relativePath.replace('.md', '')">
-        <a :href="posts[i - 1].url">{{ "<< " + posts[i - 1].frontmatter.title }}</a>
-      </div>
-      <div class="navigation-button next" v-if="i + 1 in posts && posts[i + 1].frontmatter.title && post.url.replace('/', '') == page.relativePath.replace('.md', '')">
-        <a :href="posts[i + 1].url">{{ posts[i + 1].frontmatter.title + " >>" }}</a>
-      </div>
-    </span>
+    <div class="blog-navigation" v-for="(post, i) in posts">
+      <a class="previous-post" v-if="i - 1 in posts && posts[i - 1].frontmatter.title && post.url.replace('/', '') == page.relativePath.replace('.md', '')" :href="posts[i - 1].url">{{ "<< " + posts[i - 1].frontmatter.title }}</a>
+      <a class="next-post" v-if="i + 1 in posts && posts[i + 1].frontmatter.title && post.url.replace('/', '') == page.relativePath.replace('.md', '')" :href="posts[i + 1].url">{{ posts[i + 1].frontmatter.title + " >>" }}</a>
+    </div>
   </div>
 </template>
 

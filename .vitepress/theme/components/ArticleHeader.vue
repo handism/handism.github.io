e151ce0a21a8026bc452600839e9d80563123c65
handism <andy2roidy@gmail.com>
2024-01-05 21:45:14 +0900

    日付を0埋めに変更

--- .vitepress/theme/components/ArticleHeader.vue
+++ .vitepress/theme/components/ArticleHeader.vue
@@ -6,7 +6,7 @@ const { frontmatter, page } = useData()
 
 <template>
   <div class="entry-meta">
-    <time v-if="frontmatter.title">{{ new Date(page.lastUpdated).toLocaleDateString({timeZone: 'Asia/Tokyo'}) }}</time>
+    <time v-if="frontmatter.title">{{ new Date(page.lastUpdated).toLocaleDateString("ja-JP", {year: "numeric",month: "2-digit", day: "2-digit"}) }}</time>
     <h1 v-if="frontmatter.title" class="entry-title">{{ frontmatter.title }}</h1>   
     <span v-for="tag in frontmatter.tags" class="tags">
       <a :href="`/tag/${tag}`" class="tag">{{ "# " + tag }}</a>

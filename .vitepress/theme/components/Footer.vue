3f7cbc73179c7c875cf1eea846bf7654474a17af
handism <andy2roidy@gmail.com>
2024-01-05 21:58:28 +0900

    フッタにリンク追加

--- .vitepress/theme/components/Footer.vue
+++ .vitepress/theme/components/Footer.vue
@@ -6,6 +6,11 @@ const { theme } = useData()
 
 <template>
   <footer class="footer">
+    <div class="footer-links">
+      <a href="/about">このブログについて</a>|
+      <a href="">サイトマップ</a>|
+      <a href="">プライバシーポリシー</a>
+    </div>
     <small>&copy; {{ new Date().getFullYear() + " " + theme.siteTitle }}</small>
   </footer>
 </template>

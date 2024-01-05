e0dd2c3a2cc37fabc3dd94e654c06d6f9ea45f6f
handism <andy2roidy@gmail.com>
2024-01-06 00:07:46 +0900

    修正

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -12,7 +12,14 @@ export default defineConfig({
           ["link", {rel: "icon", href: "/favicon.ico"}],
           ["link", {rel: "preconnect", href: "https://fonts.googleapis.com"}],
           ["link", {rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: ""}],
-          ["link", {href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;700&display=swap", rel: "stylesheet"}]
+          ["link", {href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;700&display=swap", rel: "stylesheet"}],
+          ["script", {async: "", src: "https://www.googletagmanager.com/gtag/js?id=G-MSJLEN90KY"}],
+          ["script", {},
+            `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MSJLEN90KY');`
+          ]
         ],
   lastUpdated: true,
   sitemap: {hostname: "https://handism.github.io"},

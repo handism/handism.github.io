079b53dcdd806a2d9e05a8278eb7b2be7bdcb91f
handism <andy2roidy@gmail.com>
2024-01-05 23:46:51 +0900

    修正

--- .vitepress/theme/components/ArticleFooter.vue
+++ .vitepress/theme/components/ArticleFooter.vue
@@ -8,11 +8,11 @@ const { page } = useData()
   <div class="entry-footer">
     <div class="blog-navigation">
       <template v-for="(post, i) in posts">
-        <a class="previous-post" v-if="i - 1 in posts && posts[i - 1].frontmatter.title && post.url.replace('/', '') == page.relativePath.replace('.md', '')" :href="posts[i - 1].url">{{ "<< " + posts[i - 1].frontmatter.title }}</a>
+        <a class="previous-post" v-if="i - 1 in posts && posts[i - 1].frontmatter.tags && post.url.replace('/', '') == page.relativePath.replace('.md', '')" :href="posts[i - 1].url">{{ "<< " + posts[i - 1].frontmatter.title }}</a>
       </template>
       <div class="cushion"></div>
       <template v-for="(post, i) in posts">
-        <a class="next-post" v-if="i + 1 in posts && posts[i + 1].frontmatter.title && post.url.replace('/', '') == page.relativePath.replace('.md', '')" :href="posts[i + 1].url">{{ posts[i + 1].frontmatter.title + " >>" }}</a>
+        <a class="next-post" v-if="i + 1 in posts && posts[i + 1].frontmatter.tags && post.url.replace('/', '') == page.relativePath.replace('.md', '')" :href="posts[i + 1].url">{{ posts[i + 1].frontmatter.title + " >>" }}</a>
       </template>
     </div>
   </div>

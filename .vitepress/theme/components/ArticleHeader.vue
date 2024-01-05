bb9666ce97969830d83b0ecee91cdfa6f2616324
handism <andy2roidy@gmail.com>
2024-01-05 23:49:38 +0900

    修正

--- .vitepress/theme/components/ArticleHeader.vue
+++ .vitepress/theme/components/ArticleHeader.vue
@@ -6,8 +6,8 @@ const { frontmatter, page } = useData()
 
 <template>
   <div class="entry-meta">
-    <time v-if="frontmatter.title">{{ new Date(page.lastUpdated).toLocaleDateString("ja-JP", {year: "numeric",month: "2-digit", day: "2-digit"}) }}</time>
-    <h1 v-if="frontmatter.title" class="entry-title">{{ frontmatter.title }}</h1>   
+    <time v-if="frontmatter.tags">{{ new Date(page.lastUpdated).toLocaleDateString("ja-JP", {year: "numeric",month: "2-digit", day: "2-digit"}) }}</time>
+    <h1 v-if="frontmatter.tags" class="entry-title">{{ frontmatter.title }}</h1>   
     <span v-for="tag in frontmatter.tags" class="tags">
       <a :href="`/tag/${tag}`" class="tag">{{ "# " + tag }}</a>
     </span>

3da9894976542a216a8d54b30d6b976b89a847c7
handism <andy2roidy@gmail.com>
2024-01-05 22:11:35 +0900

    テーブルのスタイリング変更

--- .vitepress/theme/components/Footer.vue
+++ .vitepress/theme/components/Footer.vue
@@ -7,9 +7,9 @@ const { theme } = useData()
 <template>
   <footer class="footer">
     <div class="footer-links">
-      <a href="/about">このブログについて</a>|
-      <a href="">サイトマップ</a>|
-      <a href="">プライバシーポリシー</a>
+      <a href="/about">about</a>|
+      <a href="">sitemap</a>|
+      <a href="">privacy policy</a>
     </div>
     <small>&copy; {{ new Date().getFullYear() + " " + theme.siteTitle }}</small>
   </footer>

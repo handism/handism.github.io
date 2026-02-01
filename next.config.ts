f402544029a869a28c5ed8e5c4039506d5627ce8
handism <handism@users.noreply.github.com>
2026-02-01 23:08:36 +0900

    エラーの修正

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   trailingSlash: true,
+  images: {
+    unoptimized: true, // 静的エクスポート時は必須
+  },
 };
 
 export default nextConfig;

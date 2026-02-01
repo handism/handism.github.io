a4d091a5facb6746234cdf0517349bec7eea459b
handism <handism@users.noreply.github.com>
2026-02-01 18:55:29 +0900

    パラメータ修正

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -23,8 +23,7 @@ export default function SearchBox({ posts }: Props) {
           { name: 'category', weight: 0.5 },
         ],
         threshold: 0.3,
-        ignoreLocation: true, // 部分一致を許可
-        minMatchCharLength: 1, // 1文字からマッチ
+        ignoreLocation: true,
       }),
     [posts]
   );

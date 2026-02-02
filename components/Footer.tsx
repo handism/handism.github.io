8873a7b4fb06a808e249fe05ca2ed12f4f31c939
handism <handism@users.noreply.github.com>
2026-02-02 23:15:50 +0900

    レイアウト修正

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-card py-4">
-      <div className="max-w-6xl mx-auto px-4 text-sm text-text/70">
+    <footer className="bg-card py-6">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 text-sm text-text/70">
         <div className="flex flex-col items-center space-y-4">
           <p className="text-center">
             &copy; {new Date().getFullYear()} Handism&apos;s Tech Blog. All rights reserved.

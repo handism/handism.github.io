f013558d69e141d79b765e9607df4f6bc15fdc28
handism <handism@users.noreply.github.com>
2026-02-05 23:39:30 +0900

    CSS設定

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,17 @@
-import type { Config } from "tailwindcss";
-import typography from "@tailwindcss/typography";
+import type { Config } from 'tailwindcss';
+import typography from '@tailwindcss/typography';
 
 const config: Config = {
-  darkMode: "class",
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,md,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: 'class',
+  content: ['./app/**/*.{js,ts,jsx,tsx,md,mdx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        "bg": "var(--bg)",        // 背景
-        "card": "var(--card)",    // カードやサイドバー
-        "text": "var(--text)",    // 文字
-        "border": "var(--border)",// 境界線
+        bg: 'var(--bg)', // 背景
+        card: 'var(--card)', // カードやサイドバー
+        text: 'var(--text)', // 文字
+        border: 'var(--border)', // 境界線
+        accent: 'var(--accent)', // アクセントカラー
       },
     },
   },
